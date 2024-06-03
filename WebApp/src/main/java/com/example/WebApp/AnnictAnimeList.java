0139549c17a6c4562593f8874fd39c03ea8e7aa5
Shion <shion@example.com>
2024-06-03 11:15:56 +0900

    commit

---
+++ WebApp/src/main/java/com/example/WebApp/AnnictAnimeList.java
@@ -0,0 +1,16 @@
+package com.example.WebApp;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class AnnictAnimeList {
+
+}
